464db877d262746a5377795538c65173441a6816
vuedrills <reefai.us@gmail.com>
2024-04-26 20:32:17 +0100

    meta description

--- src/config.js
+++ src/config.js
@@ -6,9 +6,8 @@ module.exports.SITE = {
   trailingSlash: false,
 
   title: 'Waterfalls Medical Center Your Health Is Our Priority',
-  description: 'Waterfalls Medical Center Your Health Is Our Priority',
+  description: 'Waterfalls Medical Center, is located at corner Parktown and Masotcha Ndlovhu road near Parktown Hospital. We offer Family Planning Services, Baby Immmunisation, Vaccine And Travel Clinic, Laboratory, Pap smears',
 };
 
 //'https://images.unsplash.com/photo-1612776572997-76cc42e058c3'
 //image: 'https://images.unsplash.com/photo-1631217871099-88310a909a32'
-//image: 'https://images.unsplash.com/photo-1605377347958-e8bd4c00ba1d'
